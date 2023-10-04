ab6ac9ef0fe2fd68ef30059520769f581c455bad
Celestine Kariuki <celestino@safariblocks.com>
2023-10-04 17:27:41 +0000

    escape seq ex1

---
+++ lec05/exercise01/escape1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    cout << "Strathmore University is the leading" << "\n";
+    cout << "university in the region" << endl;
+
+    return 0;
+}
