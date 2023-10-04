a051e900fe70a460a96b296b0b21869ace5980ea
Celestine Kariuki <celestino@safariblocks.com>
2023-10-04 17:08:06 +0000

    escape sequence first practice

---
+++ lec05/exercise03/EscapeSequence.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+/*
+
+CLASS EXERCISE II:
+
+Write a program that will output the statements below as they appear.
+Hint: Tab spaces were used to indent the 3 inner statements
+
+ DBIT is a university diploma designed to focus on:
+	1. Development
+	2. Implementation, and
+	3. Management of information systems.
+
+*/
+
+using namespace std;
+
+int main() {
+
+    cout << "*****\"Escape Sequence Practice\"*****" << "\n";
+    cout << "DBIT is a university diploma designed to focus on:" << "\n";
+    cout << "\t1.Development" << "\n";
+    cout << "\t2.Implementation, and" << "\n";
+    cout << "\t3.Management of information systems." << endl;
+
+    return 0;
+}
