5e7e07a0ffa10ad0da6cc6e5d9ab1b3e62858b05
Celestine Kariuki <celestino@safariblocks.com>
2023-10-04 17:27:14 +0000

    Escape Sequence exercise 02

---
+++ lec05/exercise02/escape2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+/*
+
+CLASS EXERCISE I
+
+Write a C++ program that will output the following statements as they appear:
+
+"
+Strathmore University Vision
+…………………………………
+To become a leading out- come driven entrepreneurial
+research University by translating our excellence into
+major contribution to culture, economic well-being and
+quality of life.
+"
+
+*/
+
+using namespace std;
+
+int main() {
+
+    cout << "Strathmore University Vision" << "\n";
+    cout << "............................" << "\n";
+    cout << "To become a leading out- come driven entrepreneurial" << "\n";
+    cout << "research University by translating our excellence into" << "\n";
+    cout << "major contribution to culture, economic well-being and" << "\n";
+    cout << "quality of life." << endl;
+
+    return 0;
+}
