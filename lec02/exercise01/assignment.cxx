ac9e651641c44dc353686d83c818475b1f73c04d
Celestine Kariuki <celestino@safariblocks.com>
2023-10-10 19:41:46 +0000

    assignment, output formatted statements

---
+++ lec02/exercise01/assignment.cxx
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to the wonderful world of C++! \n";
+    cout << "This is my first C++ statement \n";
+    cout << "Soon, I will learn how to input data into my \n";
+    cout << "program. \n";
+
+    return 0;
+}
