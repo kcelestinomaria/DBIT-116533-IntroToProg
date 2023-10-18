403ed8849573d9337aa2dc54d63902fb9cf79ce4
Celestine Kariuki <celestino@safariblocks.com>
2023-10-18 16:07:50 +0000

    calc rectangle area

---
+++ lec04/variables.cxx
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/*
+Class Exercise 1
+*/
+using namespace std;
+
+int main() {
+
+    // declare variables here
+    int area, length, width;
+
+    // next initialize the variables
+    length = 8;
+    width = 5;
+
+    // now compute the area
+    area = length * width;
+
+    // lastly, output the area
+    cout << "Area of the rectangle is: " << area << endl;
+    return 0;
+}
