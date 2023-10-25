9b01580cca3cd3de64eef554dde78a9b7df8dbea
Celestine Kariuki <celestino@safariblocks.com>
2023-10-25 15:37:45 +0000

    simple boolean example

---
+++ lec05/exercise01/booleanexample.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    // declare and initialize a boolean variable
+    bool notTrue = 0;
+
+    // output the value of the variable
+    cout << "Out put = " << notTrue << endl;
+    return 0;
+}
