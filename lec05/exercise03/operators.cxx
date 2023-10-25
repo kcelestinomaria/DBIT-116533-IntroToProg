ca6e9292aeca746179bb7d469d16366df61ce53e
Celestine Kariuki <celestino@safariblocks.com>
2023-10-25 17:16:51 +0000

    add comparison & boolean operators

--- lec05/exercise03/operators.cxx
+++ lec05/exercise03/operators.cxx
@@ -3,13 +3,26 @@
 */
 
 #include <iostream>
+#include <string>
 
 using namespace std;
+
+/*
+void returnBoolean(int booleanNum) {
+    if (booleanNum == 0) {
+        return false;
+    } else
+        return true;
+    return booleanNum;
+}
+*/
 
 int main() {
 
     // This is how "Arithmetic Operators" work
     int num1 = 10, num2 = 30; // Two 'integer' variables declared
+
+    bool var1 = true, var2 = false; // Declare and initialize 'Boolean' variables
 
     // 1 - Addition Arithmetic Operator, i.e '+'
     cout << "Sum of two numbers(num1 & num2) is: " << num1 + num2 << endl;
@@ -27,7 +40,7 @@ int main() {
     cout << "Remainder when both numbers are divided by each other: " << num1 % num2 << endl;
 
     // 6 - Increment operator, i.e '++'
-    cout << "Incrememnt num1 by 1: " << ++num1 << endl;
+    cout << "Increment num1 by 1: " << ++num1 << endl;
 
     // 7 - Decrement operator, i.e '--'
     cout << "Decrement num1 by 1: " << --num1 << endl;
@@ -35,5 +48,33 @@ int main() {
     /*
     * Comparison operators 
     */
+    // 1 - Greater than i.e '>'
+    cout << num1 << " is greater than " << num2 << " returns " <<  (num1 > num2) << endl;
+
+    // 2 - Less than i.e '<'
+    cout << num1 << " is lesser than " << num2 << " returns " << (num1 < num2) << endl;
+
+    // 3 - Equal to i.e '=='
+    cout << num1 << " is equal to " << num2 << " returns " << (num1 == num2) << endl;
+
+    // 4 - 'Not Equal' to i.e '!='
+    cout << num1 << " is not equal to " << num2 << " returns " << (num1 != num2) << endl;
+
+    cout << "\n\n";
+    cout << "Note that 0 means \"Boolean False\" , 1 means \"Boolean True\"" << endl;
+    cout << "\n\n";
+
+    /*
+    * Boolean operators
+    */
+    // 1 - AND i.e '&&'
+    cout << var1 << " && " << var2 << " returns " << (var1 && var2) << endl;
+
+    // 2 - OR i.e '||'
+    cout << var1 << " || " << var2 << " returns " << (var1 || var2) << endl;
+
+    // 3 - Boolean i.e '|'
+    cout << var1 << " | " << var2 << " returns " << (var1 | var2) << endl;
+
     return 0;
 }
