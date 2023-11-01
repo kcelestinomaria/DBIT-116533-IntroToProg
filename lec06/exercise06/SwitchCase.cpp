1aee4d3bc258802a9119d7d7a07c510009f0c940
Celestine Kariuki <celestino@safariblocks.com>
2023-11-01 17:33:21 +0000

    Incomplete switch case

---
+++ lec06/exercise06/SwitchCase.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/*
+* This program demonstrates how "switch...case" is used
+* as a decision/conditional structure in C++
+*/
+
+using namespace std;
+
+int main() {
+
+    int opt;
+
+    cout << "Hi! This is your DBIT Sem 1 Timetable" << endl;
+    cout << "\n1. Monday\n2. Tuesday\n3. Wednesday\n4. Thursday\n 5. Friday" << endl << endl;
+    cout << "Please select day of choice (e.g. 1, 2, 3, etc...)" << endl;
+    cin >> opt;
+    switch (opt) {
+        case 1:
+        cout << "Monday Classes\n"
+    }
+}
