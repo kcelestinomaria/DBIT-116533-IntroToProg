b4b70bde593dbef0b8b4a082dbbf27085ff394a8
Celestine Kariuki <celestino@safariblocks.com>
2023-11-08 17:32:29 +0000

    for loop

--- lec07/exercise06/forloop.cpp
+++ lec07/exercise06/forloop.cpp
@@ -3,5 +3,11 @@
 using namespace std;
 
 int main() {
-    
+
+    cout << "A simple program to output 12, 14, 16, 18, 20, 22, 24, 26, 28 on the console" << endl;
+    cout << "Using a For Loop\n";
+
+    for (int counter = 12; counter <= 28; counter += 2 ) {
+        cout << counter << endl;
+    }
 }
