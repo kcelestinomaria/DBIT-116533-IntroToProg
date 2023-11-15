a3c5c1b88869b3cd929ecd097ef8c6e604c296b7
Celestine Kariuki <celestino@safariblocks.com>
2023-11-15 17:15:03 +0000

    vowel

--- catTwo/questionTwo/numberTwo/character.cpp
+++ catTwo/questionTwo/numberTwo/character.cpp
@@ -13,20 +13,30 @@ int main() {
     if (isalpha(inputCharacter) == false) {
         cerr << "This is not a letter!" << endl;
         return 1;
-    }
-    char lowerChar = tolower(inputCharacter);
+    } else {
+        char lowerChar = tolower(inputCharacter);
 
+    /*
     // Create array to hold our vowels
     char Vowels[5] = {'a', 'e', 'i', 'o', 'u'};
-
+    
     // Loop through Vowels array, check if vowel
     for (int i : Vowels) {
-        if (Vowels[i] == lowerChar) {
+        if ((Vowels[i] == lowerChar) == true) {
             cout << "This is a vowel: " << lowerChar << endl;
         } else {
             cout << "This is a consonant: " << lowerChar << endl;
         }
     }
+    }
 
+    */
+    if (lowerChar == 'a' || lowerChar == 'e' || lowerChar == 'i' || lowerChar == 'o' || lowerChar == 'u') {
+        cout << "this is a vowel" << endl;
+    } else {
+        cout << "this is a constant" << endl;
+    }
+    }
+    
     return 0;
 }
