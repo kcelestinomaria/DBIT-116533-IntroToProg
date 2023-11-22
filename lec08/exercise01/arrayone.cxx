27eff2166de7c53f0057b0b8045eebc20c70697d
Celestine Kariuki <celestino@safariblocks.com>
2023-11-22 16:58:37 +0000

    extra on arrays

---
+++ lec08/exercise01/arrayone.cxx
@@ -0,0 +1,16 @@
+#include <iostream>
+
+/* ~~ARRAYS IN C++
+*
+*/
+
+using namespace std;
+
+
+int main() {
+    double nums [] = {12.25, 32.50, 16.90, 23, 45.68, 50.60};
+
+    cout << nums;
+    
+    return 0;
+}
