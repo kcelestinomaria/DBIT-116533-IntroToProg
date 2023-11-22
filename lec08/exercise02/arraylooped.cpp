3c2d50f22dcc9219a1b64e229e50bfd0edb5401c
Celestine Kariuki <celestino@safariblocks.com>
2023-11-22 15:39:34 +0000

    while

---
+++ lec08/exercise02/arraylooped.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int nums[5];
+    int counter = 0;
+    while(counter < 6) {
+        cin >> nums[counter];
+        counter++;
+    }
+    return 0;
+}
