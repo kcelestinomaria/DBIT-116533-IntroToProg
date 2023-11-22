aa1b1bcbb2af54637039692e33ff2dab66b0c594
Celestine Kariuki <celestino@safariblocks.com>
2023-11-22 16:28:19 +0000

    array sums, sizeof and no. of elements

--- lec08/exercise02/arraylooped.cpp
+++ lec08/exercise02/arraylooped.cpp
@@ -4,11 +4,35 @@ using namespace std;
 
 int main() {
 
-    int nums[5];
+    // declare and initialize the integer array, and a sum variable
+    int nums[5], sum = 0;
+
+    // initialize the counter variable, to loop through the array's indices
     int counter = 0;
-    while(counter < 6) {
+
+    cout << "Please enter 5 numbers: " << endl;
+
+    // using a while loop, go through the the array
+    while(counter < 5) {
+
+        // receive from the user each of the array's values to be held in memory
         cin >> nums[counter];
+
+        // increment the counter, to go through the array from the first index onwards
         counter++;
     }
+
+
+    cout << "\n";
+    cout << "Here is the array's output: " << endl;
+    for (int iterator = 0; iterator < 5; ++iterator) {
+        // sum = sum + nums[iterator]
+        sum += nums[iterator];
+    }
+    cout << "\n";
+    cout << "The sum is: " << sum << endl;
+    cout << "The byte size of the array, in total is: " << sizeof(nums) << endl;
+    cout << "The number of elements in our array is: " << sizeof(nums) / sizeof(int) << endl;
+
     return 0;
 }
