1107947d87a47be50f2e09bda0827a92dae2c90b
Celestine Kariuki <celestino@safariblocks.com>
2023-11-29 15:56:45 +0000

    functions, but in separate files

---
+++ lec09/exercise01/functions.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "functions.h"
+
+using namespace std;
+
+int main() {
+
+    greetings();
+    
+    addTwoNumbers(2, 5);
+
+    subtractTwoNumbers(2, 5);
+
+    return 0;
+}
---
+++ lec09/exercise01/functions.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void greetings() {
+    cout << "Hi there! Hola! Bonjour! Habari Yako! " << endl;
+}
+
+int addTwoNumbers(int num_1, int num_2) {
+    int sum = num_1 + num_2;
+    return sum;
+}
+
+int subtractTwoNumbers(int num_1, int num_2) {
+    int subtract = num_2 - num_1;
+    return subtract;
+}
